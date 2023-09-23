eee8aca6604cdd2abc5f4cb86e0a91c1f90e53fb
at47356 <101987474+at47356@users.noreply.github.com>
2023-09-23 20:58:18 +0100

    Add timer which starts at first click and resets at new game

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ export default function App() {
 
   const [randomDiceNumbers, setRandomDiceNumber] = useState(allNewDice)
   const [tenzies, setTenzies] = useState(false)
+  const [timer, setTimer] = useState({
+    millisec: 0,
+    seconds: 0,
+    minutes: 0,
+  });
+  const [timerId, setTimerId] = useState(null);
+  const [gameStarted, setGameStarted] = useState(false)
 
   useEffect(() => {
     const allHeld = randomDiceNumbers.every(die => die.isHeld )
@@ -17,15 +24,53 @@ export default function App() {
     if(allEqual && allHeld){
       setTenzies(true)
       console.log("You Won!!")
+      // clearInterval(timerId);
+      // setTimerId(null);
     }
-  },[randomDiceNumbers])
+    tenzies && clearInterval(timerId);
+    
+  },[randomDiceNumbers, tenzies, timerId])
+
+
+
+  function startTimer() {
+    if (gameStarted == false) {
+      setGameStarted(true)
+      // Clear any previous interval
+      clearInterval(timerId);
+
+      // Set the timer to increment every 10 milliseconds
+      const newTimerId = setInterval(() => {
+        setTimer((prevTimer) => {
+          let { millisec, seconds, minutes } = prevTimer;
+          millisec += 10;
+
+          millisec === 1000 && (millisec = 0, seconds++);
+          seconds === 60 && (seconds = 0, minutes++); 
+          return {
+            minutes,
+            seconds,
+            millisec,
+          };
+        });
+      }, 10);
+
+      // Save the timer ID in state for later use
+      setTimerId(newTimerId);
+    }
+  }
+
+
 
   function hold(id) {
     setRandomDiceNumber(
       oldDice => oldDice.map(die => (
           die.id === id ? {...die, isHeld: !die.isHeld} : die
       ))
-  )
+    )
+    if (gameStarted == false) {
+      startTimer();
+    }   
   }
 
   const dies = randomDiceNumbers.map((object, index) => (
@@ -34,6 +79,7 @@ export default function App() {
       value={object.value}
       isHeld={object.isHeld}
       hold={()=> hold(object.id)}
+      onClick={() => handleDieClick(object.id)}
     />
   ));
 
@@ -60,6 +106,7 @@ export default function App() {
   }
 
   function newGame() {
+    setGameStarted(false)
     setTenzies(false)
     setRandomDiceNumber(oldDice => oldDice.map(die => (
       {
@@ -68,6 +115,13 @@ export default function App() {
         id: nanoid()
       }
     )))
+    clearInterval(timerId);
+    setTimerId(null);
+    setTimer({
+      millisec: 0,
+      seconds: 0,
+      minutes: 0,
+    })
   }
   
 
@@ -77,7 +131,11 @@ export default function App() {
       {tenzies && <Confetti/>}
       </div>
       <main>
-          
+          <div className="timer">
+            <span className="ms">{timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes}</span>:
+            <span className="sec">{timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds}</span>:
+            <span className="min">{timer.millisec < 10 ? `00${timer.millisec}` : timer.millisec < 100 ? `0${timer.millisec}` : timer.millisec}</span>
+          </div>
           <h1 className="title">Tenzies</h1>
           <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
           <div className="dice--container">
